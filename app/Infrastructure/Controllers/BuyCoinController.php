18bfebc638e25239140946b0f688308d6a75763d
Ander Flores <flores.ander29@gmail.com>
2022-05-16 09:49:10 +0200

    BuyCoinController

--- app/Infrastructure/Controllers/BuyCoinController.php
+++ app/Infrastructure/Controllers/BuyCoinController.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Controllers;
 
+use App\Application\BuyCoin\BuyCoinService;
 use App\Application\GetCoin\GetCoinService;
 use Barryvdh\Debugbar\Controllers\BaseController;
 use Exception;
@@ -24,40 +25,30 @@ class BuyCoinController extends BaseController
     {
         if (!$request->has("coin_id")) {
             return response()->json([
-                'error' =>'coin_id not introduced'
+                'error' => 'coin_id not introduced'
             ], Response::HTTP_BAD_REQUEST);
-        } elseif(!$request->has("wallet_id")) {
+        } elseif (!$request->has("wallet_id")) {
             return response()->json([
-                'error' =>'wallet_id not introduced'
+                'error' => 'wallet_id not introduced'
             ], Response::HTTP_BAD_REQUEST);
-        } elseif(!$request->has("amount_usd")) {
+        } elseif (!$request->has("amount_usd")) {
             return response()->json([
                 'error' => 'amount_usd not introduced'
             ], Response::HTTP_BAD_REQUEST);
         }
-        return response()->json([
-            'bien' => 'bien'
-        ], Response::HTTP_ACCEPTED);
-        /*
         try {
-            $coin = $this->buyCoinService->execute($request);
+            $bought_amount = $this->buyCoinService->execute($request->input("coin_id"), $request->input("amount_usd"));
         } catch (ServiceUnavailableHttpException $exception) {
             return response()->json([
                 'error' => $exception->getMessage()
             ], Response::HTTP_SERVICE_UNAVAILABLE);
         } catch (NotFoundHttpException $exception) {
             return response()->json([
-                'error' =>'A coin with the specified ID was not found'
+                'error' => 'A coin with the specified ID was not found'
             ], Response::HTTP_NOT_FOUND);
         }
         return response()->json([
-            'coin_id' => $coin->getCoin_id(),
-            'symbol' => $coin->getSymbol(),
-            'name' => $coin->getName(),
-            'name_id' => $coin->getName_id(),
-            'rank' => $coin->getRank(),
-            'price_usd' => $coin->getPrice_usd()
+            "bought_amount" => $bought_amount
         ], Response::HTTP_OK);
-        */
     }
 }
